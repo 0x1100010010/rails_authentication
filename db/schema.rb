0a7d1899529ff25a9ce785fdf49691d268f0dc67
bashforger <bash.forger@gmail.com>
2021-01-22 20:14:14 +0500

    Added root routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_22_150012) do
+
+  create_table "sign_ups", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
